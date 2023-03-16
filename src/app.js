59620ab0a9e66074890ba291565f51bd9e7f997c
Blake Sanders <blakesanders7@gmail.com>
2023-03-16 10:33:18 -0400

    Light and Dark mode achieved, no button transition coded yet

--- src/app.js
+++ src/app.js
@@ -6,3 +6,9 @@ function toggleButton() {
 }
 
 hamburgerButton.addEventListener('click', toggleButton)
+
+const switchElement = document.querySelector('.switch')
+
+switchElement.addEventListener('click', () => {
+    document.body.classList.toggle('dark')
+})
